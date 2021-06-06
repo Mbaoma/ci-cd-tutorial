97e73f010260badb3641d965279775f16d65a8ef
Mbaoma <mmaryraphaella@gmail.com>
2021-06-06 11:50:16 +0100

    test branch protectn

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ class TestSum(unittest.TestCase):
     def test_sum_tuple(self):
         self.assertEqual(sum((1, 2, 2)), 5, "Should be 5")
 
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 2)), 5, "Should be 5")
 
 if __name__ == '__main__':
     unittest.main()
